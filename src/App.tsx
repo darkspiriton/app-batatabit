ca5aa9aca1346c42296d08e7b7a27c4b143a4717
Cesar Moreno <cesar200526@gmail.com>
2023-08-24 22:42:05 -0500

    style: :white_check_mark: Se agrego texto header inicial

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import "./App.scss";
 function App() {
   return (
     <>
-      <header></header>
+      <header>
+        <img src="" alt="" />
+        <div>
+          <h1>La próxima revolución en el intercambio de criptomonedas.</h1>
+          <p>
+            Batatabit te ayuda a navegar entre los diferentes precios y
+            tendencias.
+          </p>
+          <a href="">Conoce Nuestros Planes</a>
+        </div>
+      </header>
       <main>
         <section></section>
         <section></section>
