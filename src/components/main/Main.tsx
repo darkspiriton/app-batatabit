a9287afd0c357a1b4724b901ecc360caa589cba1
Cesar Moreno <cesar200526@gmail.com>
2023-09-05 23:12:14 -0500

    feat: :rocket: Se agrego maqueta de tarjeta de beneficios

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -105,8 +105,53 @@ function Main() {
             </>
           )}
         </section>
-
-        <section></section>
+        <section className="main-product-detail">
+          <span className="product-detail--batata-logo"></span>
+          <div className="product-detail--title">
+            <h2>Creamos un producto sin comparación.</h2>
+            <p>Confiable y diseñado para su uso diario.</p>
+          </div>
+          <section className="product-cards--container">
+            <article className="product-detail--card">
+              <span className="clock"></span>
+              <p className="product--card-title">Tiempo real</p>
+              <p className="product--card-body">
+                Nuestro API toma información minuto a minuto sobre las tasas que
+                más determinan el comportamiento.
+              </p>
+            </article>
+          </section>
+          <section className="product-cards--container">
+            <article className="product-detail--card">
+              <span className="eye"></span>
+              <p className="product--card-title">No hay tasas escondidas</p>
+              <p className="product--card-body">
+                Ni en la compra o al momento de exit, Batabit siempre te muestra
+                el costo real de lo que estás adquiriendo.
+              </p>
+            </article>
+          </section>
+          <section className="product-cards--container">
+            <article className="product-detail--card">
+              <span className="dollar"></span>
+              <p className="product--card-title">Compara monedas</p>
+              <p className="product--card-body">
+                No más rumores, con Babtabit sabrás el valor real de cada moneda
+                en el mercado actual.
+              </p>
+            </article>
+          </section>
+          <section className="product-cards--container">
+            <article className="product-detail--card">
+              <span className="check"></span>
+              <p className="product--card-title">Información confiable</p>
+              <p className="product--card-body">
+                Nuestras fuentes están 100% verificadas y continuamos auditando
+                su contenido mientras se actualizan.
+              </p>
+            </article>
+          </section>
+        </section>
         <section></section>
       </main>
     </>
