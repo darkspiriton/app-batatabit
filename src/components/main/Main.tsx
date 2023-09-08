caf7c016df5dfb912a4e6fd822519557e96c93af
Cesar Moreno <cesar200526@gmail.com>
2023-09-07 23:50:03 -0500

    Se agregaron tarjetas extra

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -177,6 +177,40 @@ function Main() {
               </div>
             </article>
           </section>
+          <section className="plans-container--slider">
+            <article className="plans-container--card">
+              <p className="recomended">Recomendado</p>
+              <div className="plan-info-container">
+                <h3 className="plan-card--title">Pago Anual</h3>
+                <p className="plan-card--price">
+                  <sup>$</sup> 99
+                </p>
+                <p className="plan-card--saving">
+                  *Ahorras $129 comparado al plan mensual.
+                </p>
+                <button className="plan-card--ca">
+                  Escoge este <span></span>
+                </button>
+              </div>
+            </article>
+          </section>
+          <section className="plans-container--slider">
+            <article className="plans-container--card">
+              <p className="recomended">Recomendado</p>
+              <div className="plan-info-container">
+                <h3 className="plan-card--title">Pago Anual</h3>
+                <p className="plan-card--price">
+                  <sup>$</sup> 99
+                </p>
+                <p className="plan-card--saving">
+                  *Ahorras $129 comparado al plan mensual.
+                </p>
+                <button className="plan-card--ca">
+                  Escoge este <span></span>
+                </button>
+              </div>
+            </article>
+          </section>
         </section>
       </main>
     </>
