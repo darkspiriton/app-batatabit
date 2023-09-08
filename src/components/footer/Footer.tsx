e723e58b864235798c8c813bdc3bee520b76fe61
Cesar Moreno <cesar200526@gmail.com>
2023-09-08 01:52:47 -0500

    style: :rocket: Se agrego el footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,26 @@
 import "./Footer.scss";
+import logoFooter from "../../assets/imgs/logo-footer.svg";
 function Footer() {
   return (
     <>
-      <footer></footer>
+      <footer>
+        <section className="left">
+          <ul>
+            <li>
+              <a href="#">Linkedin</a>
+            </li>
+            <li>
+              <a href="#">Crunchbase</a>
+            </li>
+            <li>
+              <a href="#">Hackernews</a>
+            </li>
+          </ul>
+        </section>
+        <section className="right">
+          <img src={logoFooter} alt="Logo Batatabit"></img>
+        </section>
+      </footer>
     </>
   );
 }
